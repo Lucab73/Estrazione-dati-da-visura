59df313ba42f51634befea6aa04d2b1a70b48f8e
Lucab73 <lucasec73@gmail.com>
2024-12-04 08:04:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -278,10 +278,10 @@ def estrai_dati(filepath):
         if all (lettera in terza_parola for lettera in successive_3_lettere):
             return False
 
-        # Verifica se successive_3_lettere sono nella seconda parola o quinto_sesto_carattere nella terza
-        if all (lettera in seconda_parola for lettera in successive_3_lettere) or \
-                all (lettera in terza_parola for lettera in quinto_sesto_carattere): or \
-                    all (lettera in seconda_parola for lettera in quarto_carattere):
+        # Verifica se successive_3_lettere sono nella seconda parola, quinto_sesto_carattere nella terza, o quarto_carattere nella seconda
+        if all(lettera in seconda_parola for lettera in successive_3_lettere) or \
+                all(lettera in terza_parola for lettera in quinto_sesto_carattere) or \
+                all(lettera in seconda_parola for lettera in quarto_carattere):  # Rimosso l'or superfluo
             return True  # Nome correttamente separato
         else:
             return False  # Se non trovato in seconda o terza parola
